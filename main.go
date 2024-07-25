ace0d9fc5dbd79aae04f6ba51cb2a1f8627ae84b
Gulov Azam <gulovazam@gmail.com>
2024-07-25 14:59:42 +0500

    Setup application and server

--- main.go
+++ main.go
@@ -1,4 +1,57 @@
 package main
 
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/bookmanjunior/members-only/api"
+	"github.com/bookmanjunior/members-only/config"
+	"github.com/bookmanjunior/members-only/internal/models"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+const Red = "\033[31m"
+
 func main() {
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, Red+"ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	addr := flag.String("addr", ":3000", "HTTP network address")
+
+	flag.Parse()
+	if err := godotenv.Load(); err != nil {
+		errorLog.Fatal(err)
+	}
+
+	conncstring := fmt.Sprintf("user=%v password=%v host=%v port=%v dbname=%v sslmode=disable", os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	db, err := sql.Open("postgres", conncstring)
+
+	if err != nil {
+		errorLog.Fatal(err.Error())
+	}
+
+	app := &config.Application{
+		ErrorLog: errorLog,
+		InfoLog:  infoLog,
+		Users:    &models.UserModel{DB: db},
+		Messages: &models.MessageModel{DB: db},
+	}
+
+	server := &http.Server{
+		Handler:  api.Router(app),
+		ErrorLog: errorLog,
+		Addr:     *addr,
+	}
+
+	defer db.Close()
+
+	infoLog.Printf("Listening on port %s\n", *addr)
+	err = server.ListenAndServe()
+	errorLog.Fatal(err)
 }
