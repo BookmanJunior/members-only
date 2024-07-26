cc9ba54b6938ecd6f6254db2b79064975cd0cf1b
Gulov Azam <gulovazam@gmail.com>
2024-07-26 15:56:55 +0500

    initial commit

---
+++ internal/validator/validator.go
@@ -0,0 +1,46 @@
+package validator
+
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+type Validator struct {
+	FieldErros map[string]string
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.FieldErros) == 0
+}
+
+func (v *Validator) AddFieldError(key, message string) {
+	if v.FieldErros == nil {
+		v.FieldErros = make(map[string]string)
+	}
+
+	if _, exists := v.FieldErros[key]; !exists {
+		v.FieldErros[key] = message
+	}
+}
+
+func (v *Validator) CheckField(ok bool, key, message string) {
+	if !ok {
+		v.AddFieldError(key, message)
+	}
+}
+
+func (v *Validator) NotBlank(value string) bool {
+	return strings.TrimSpace(value) != ""
+}
+
+func (v *Validator) MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+func (v *Validator) MaxChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) < n
+}
+
+func (v *Validator) AreFieldsEquel(value1, value2 string) bool {
+	return value1 == value2
+}
