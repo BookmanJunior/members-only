ca538fea940871f529f6d3c4828dee4299933e37
Gulov Azam <gulovazam@gmail.com>
2024-07-26 15:59:58 +0500

    Add GetByUsername method
    refactor Insert method
    remove redundant error check from Get method

--- internal/models/user.go
+++ internal/models/user.go
@@ -14,17 +14,29 @@ type UserModel struct {
 	DB *sql.DB
 }
 
-func (u *UserModel) Insert(username, password string, avatar uint) {
-	u.DB.Exec(`insert into users (username, password, avatar) values ($1, $2, $3)`, username, password, avatar)
+func (u *UserModel) Insert(username, password string, avatar int) (User, error) {
+	user := User{}
+	var userId int
+	res := u.DB.QueryRow(`insert into users (username, password, avatar) values ($1, $2, $3) returning id`, username, password, avatar)
+
+	err := res.Scan(&userId)
+
+	if err != nil {
+		return user, err
+	}
+
+	user, err = u.Get(userId)
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
 }
 
 func (u *UserModel) Get(id int) (User, error) {
 	user := User{}
 	res := u.DB.QueryRow(`select "username", "password", "avatar_url" from "users" inner join "avatars" on users.avatar = avatars.id and users.id = $1`, id)
-
-	if res.Err() != nil {
-		return user, res.Err()
-	}
 
 	err := res.Scan(&user.Username, &user.password, &user.Avatar)
 
@@ -33,4 +45,18 @@ func (u *UserModel) Get(id int) (User, error) {
 	}
 
 	return user, nil
+}
+
+func (u *UserModel) GetByUsername(username string) (string, error) {
+	var usernameExists string
+	res := u.DB.QueryRow(`select "username" from "users" where "username" = $1`, username)
+
+	err := res.Scan(&usernameExists)
+
+	if err != nil {
+		return usernameExists, err
+	}
+
+	return usernameExists, nil
+
 }
