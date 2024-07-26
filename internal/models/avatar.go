81287c1233f7725470524ec7b6d759850745ff92
Gulov Azam <gulovazam@gmail.com>
2024-07-26 16:19:48 +0500

    Add AvatarModel struct
    add Get method

--- internal/models/avatar.go
+++ internal/models/avatar.go
@@ -12,6 +12,10 @@ type Avatar struct {
 	Url  string
 }
 
+type AvatarModel struct {
+	DB *sql.DB
+}
+
 func (a *Avatar) Insert(db *sql.DB) func() error {
 	return func() error {
 		if a.Url == "" || a.Name == "" {
@@ -28,4 +32,18 @@ func (a *Avatar) Insert(db *sql.DB) func() error {
 		fmt.Printf("Added %v to avatars table\n", a.Name)
 		return nil
 	}
+}
+
+func (a *AvatarModel) Get(id int) (Avatar, error) {
+	avatar := Avatar{}
+
+	res := a.DB.QueryRow(`select * from "avatars" where "id" = $1`, id)
+
+	err := res.Scan(&avatar.Name, &avatar.Url, &avatar.id)
+
+	if err != nil {
+		return avatar, err
+	}
+
+	return avatar, nil
 }
