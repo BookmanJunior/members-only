90c418c93931ef6324005b18c65d00626e6e56cd
Azam Gulov <gulovazam@gmail.com>
2024-07-29 14:21:25 +0500

    Remove test route

--- api/routes.go
+++ api/routes.go
@@ -10,9 +10,6 @@ import (
 
 func Router(app *config.Application) http.Handler {
 	router := http.NewServeMux()
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Caught"))
-	})
 	router.HandleFunc("GET /users", handlers.HandleUserGet(app))
 	router.HandleFunc("POST /users", handlers.HandleUserPost(app))
 	router.HandleFunc("GET /messages", handlers.HandleMessagesGet(app))
