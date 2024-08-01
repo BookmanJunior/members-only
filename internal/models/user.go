0e3363926e21bbb96568a946409e0027980e7942
Azam Gulov <gulovazam@gmail.com>
2024-08-01 17:37:22 +0500

    Add json fields to User struct
    remove changing username to lowercase in Insert and GetByUsername methods
    scan for admin role in queries

--- internal/models/user.go
+++ internal/models/user.go
@@ -2,52 +2,46 @@ package models
 
 import (
 	"database/sql"
-	"strings"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	Username string
-	password string
-	Avatar   string
+	Id       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"-"`
+	Avatar   string `json:"avatar"`
+	Admin    bool   `json:"admin"`
 }
 
 type UserModel struct {
 	DB *sql.DB
 }
 
-func (u *UserModel) Insert(username, password string, avatar int) (User, error) {
-	user := User{}
+func (u *UserModel) Insert(username, password string, avatar int) (int, error) {
 	var userId int
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10)
 
 	if err != nil {
-		return user, err
+		return userId, err
 	}
 
-	res := u.DB.QueryRow(`insert into users (username, password, avatar) values ($1, $2, $3) returning id`, strings.ToLower(username), hashedPassword, avatar)
+	res := u.DB.QueryRow(`insert into users (username, password, avatar) values ($1, $2, $3) returning id`, username, hashedPassword, avatar)
 
 	err = res.Scan(&userId)
 
 	if err != nil {
-		return user, err
+		return userId, err
 	}
 
-	user, err = u.Get(userId)
-
-	if err != nil {
-		return user, err
-	}
-
-	return user, nil
+	return userId, nil
 }
 
-func (u *UserModel) Get(id int) (User, error) {
+func (u *UserModel) GetById(id int) (User, error) {
 	user := User{}
-	res := u.DB.QueryRow(`select "username", "password", "avatar_url" from "users" inner join "avatars" on users.avatar = avatars.id and users.id = $1`, id)
+	res := u.DB.QueryRow(`select users.id, "username", "password", "avatar_url", "admin" from "users" inner join "avatars" on users.avatar = avatars.id and users.id = $1`, id)
 
-	err := res.Scan(&user.Username, &user.password, &user.Avatar)
+	err := res.Scan(&user.Id, &user.Username, &user.Password, &user.Avatar, &user.Admin)
 
 	if err != nil {
 		return user, err
@@ -59,9 +53,9 @@ func (u *UserModel) Get(id int) (User, error) {
 func (u *UserModel) GetByUsername(username string) (User, error) {
 	var user User
 
-	res := u.DB.QueryRow(`select "username" from "users" where "username" = $1`, strings.ToLower(username))
+	res := u.DB.QueryRow(`select "id", "username", "password", "avatar", "admin" from "users" where "username" = $1`, username)
 
-	err := res.Scan(&user.Username)
+	err := res.Scan(&user.Id, &user.Username, &user.Password, &user.Avatar, &user.Admin)
 
 	if err != nil {
 		return user, err
