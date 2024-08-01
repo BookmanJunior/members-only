f8244957948326684e3bf0d8f8bfed7e86f1e331
Azam Gulov <gulovazam@gmail.com>
2024-08-01 15:25:18 +0500

    Initial commit

---
+++ internal/auth/auth.go
@@ -0,0 +1,41 @@
+package auth
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var secretKey = []byte(os.Getenv("SECRET_TOKEN"))
+
+func CreateToken(id int) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":  id,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString(secretKey)
+
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func VerifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+	return nil
+}
