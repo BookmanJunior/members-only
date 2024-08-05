14ddd20ad364684c9641b5a10323f03e513a768a
Gulov Azam <gulovazam@gmail.com>
2024-08-05 19:01:42 +0500

    Update routes

--- api/routes.go
+++ api/routes.go
@@ -13,7 +13,9 @@ func Router(app *config.Application) http.Handler {
 	router.HandleFunc("POST /login", handlers.HandleLogin(app))
 	router.HandleFunc("GET /users", handlers.HandleUserGet(app))
 	router.HandleFunc("POST /users", handlers.HandleUserPost(app))
-	router.HandleFunc("GET /messages", handlers.HandleMessagesGet(app))
+	router.HandleFunc("GET /messages", middleware.IsAuthorized(app, handlers.HandleMessagesGet(app)))
+	router.HandleFunc("POST /messages", middleware.IsAuthorized(app, handlers.HandleMessagePost(app)))
+	router.HandleFunc("DELETE /messages/{id}", middleware.IsAuthorized(app, handlers.HandleMessageDelete(app)))
 
 	return middleware.RecoverPanic(app, middleware.Logger(app, router))
 }
