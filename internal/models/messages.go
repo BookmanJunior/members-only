124a1fd260129803e3652123576655cf176214fa
Azam Gulov <gulovazam@gmail.com>
2024-08-07 16:29:40 +0500

    Change Message struct
    change GetAll methods queryString

--- internal/models/messages.go
+++ internal/models/messages.go
@@ -10,15 +10,15 @@ type MessageModel struct {
 }
 
 type Message struct {
-	Id      int       `json:"id"`
+	Id      int       `json:"message_id"`
 	Message string    `json:"message"`
 	Time    time.Time `json:"date"`
-	User
+	User    User      `json:"user"`
 }
 
 func (m *MessageModel) GetAll() ([]Message, error) {
 	var messages []Message
-	var queryString = `select "username", "avatar_url", "message_body", date from "messages"
+	var queryString = `select messages.id , users.id, "username", "avatar_url", "message_body", date from "messages"
 	inner join "users" on messages.user_id = users.id inner join "avatars" on users.avatar = avatars.id`
 	res, err := m.DB.Query(queryString)
 
@@ -28,7 +28,7 @@ func (m *MessageModel) GetAll() ([]Message, error) {
 
 	for res.Next() {
 		message := &Message{}
-		err := res.Scan(&message.Username, &message.Avatar, &message.Message, &message.Time)
+		err := res.Scan(&message.Id, &message.User.Id, &message.User.Username, &message.User.Avatar, &message.Message, &message.Time)
 
 		if err != nil {
 			return messages, err
