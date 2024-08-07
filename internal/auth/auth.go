e8792dc1b437fca285e44dc591a3588e4a6efcbd
Azam Gulov <gulovazam@gmail.com>
2024-08-07 15:25:33 +0500

    Load env file in init function

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,10 +1,12 @@
 package auth
 
 import (
+	"log"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt"
+	"github.com/joho/godotenv"
 )
 
 type UserClaim struct {
@@ -16,7 +18,14 @@ const (
 	bearerTokenExp = 10 * time.Minute
 )
 
-var secretKey = []byte(os.Getenv("SECRET_TOKEN"))
+var secretKey []byte
+
+func init() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("env file not found")
+	}
+	secretKey = []byte(os.Getenv("TOKEN_SECRET"))
+}
 
 func CreateToken(currentUser UserClaim) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
