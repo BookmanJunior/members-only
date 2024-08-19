0caa7f48f2f93de6fe81eaf647f809a256cb1356
Gulov Azam <gulovazam@gmail.com>
2024-08-19 18:44:42 +0500

    Add get route
    wrap get users in middleware

--- api/routes.go
+++ api/routes.go
@@ -11,11 +11,12 @@ import (
 func Router(app *config.Application) http.Handler {
 	router := http.NewServeMux()
 	router.HandleFunc("POST /login", handlers.HandleLogin(app))
-	router.HandleFunc("GET /users", handlers.HandleUserGet(app))
+	router.HandleFunc("GET /users", middleware.IsAuthorized(app, handlers.HandleUserGet(app)))
 	router.HandleFunc("POST /users", handlers.HandleUserPost(app))
 	router.HandleFunc("GET /messages", middleware.IsAuthorized(app, handlers.HandleMessagesGet(app)))
 	router.HandleFunc("POST /messages", middleware.IsAuthorized(app, handlers.HandleMessagePost(app)))
 	router.HandleFunc("DELETE /messages/{id}", middleware.IsAuthorized(app, handlers.HandleMessageDelete(app)))
+	router.HandleFunc("GET /avatars", handlers.HandleGetAvatars(app))
 
 	return middleware.RecoverPanic(app, middleware.EnableCors(app, middleware.Logger(app, router)))
 }
