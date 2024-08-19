f92ff146aed268366465c15e85a0982f6b8ce7e6
Gulov Azam <gulovazam@gmail.com>
2024-08-19 18:37:16 +0500

    Refactor Avatar struct
    Remove Avatar's method
    Add GetAll method
    Refactor Exists

--- internal/models/avatar.go
+++ internal/models/avatar.go
@@ -5,33 +5,31 @@ import (
 )
 
 type Avatar struct {
-	id   int
-	Name string
-	Url  string
+	id    int
+	Color string `json:"avatar_color"`
+	Url   string `json:"avatar"`
 }
 
 type AvatarModel struct {
 	DB *sql.DB
 }
 
-func (a *Avatar) Insert(db *sql.DB) func() error {
-	return func() error {
-		_, err := db.Exec(`insert into avatars("name", "url") values($1, $2) returning id`, a.Name, a.Url)
+func (a *AvatarModel) Insert(avatar_color, avatar_url string) error {
+	_, err := a.DB.Exec(`insert into avatars("name", "url") values($1, $2)`, avatar_color, avatar_url)
 
-		if err != nil {
-			return err
-		}
-
-		return nil
+	if err != nil {
+		return err
 	}
+
+	return nil
 }
 
 func (a *AvatarModel) Get(id int) (Avatar, error) {
 	avatar := Avatar{}
 
-	res := a.DB.QueryRow(`select * from "avatars" where "id" = $1`, id)
+	res := a.DB.QueryRow(`select "avatar_color", "avatar_url", "id" from "avatars" where "id" = $1`, id)
 
-	err := res.Scan(&avatar.Name, &avatar.Url, &avatar.id)
+	err := res.Scan(&avatar.Color, &avatar.Url, &avatar.id)
 
 	if err != nil {
 		return avatar, err
@@ -40,12 +38,26 @@ func (a *AvatarModel) Get(id int) (Avatar, error) {
 	return avatar, nil
 }
 
+func (a *AvatarModel) GetAll() ([]Avatar, error) {
+	var avatars []Avatar
+
+	res, err := a.DB.Query(`select "avatar_color", "avatar_url", "id" from avatars`)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for res.Next() {
+		avatar := Avatar{}
+		res.Scan(&avatar.Color, &avatar.Url, &avatar.id)
+		avatars = append(avatars, avatar)
+	}
+
+	return avatars, nil
+}
+
 func (a *AvatarModel) Exists(id int) bool {
 	_, err := a.Get(id)
 
-	if err != nil {
-		return false
-	}
-
-	return true
+	return err == nil
 }
