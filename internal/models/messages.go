0aa8ed8c5a7c9412e2617223146c1b5f13cfa302
Gulov Azam <gulovazam@gmail.com>
2024-08-25 19:50:54 +0500

    Refactor GetAll and Get query methods

--- internal/models/messages.go
+++ internal/models/messages.go
@@ -14,16 +14,35 @@ type MessageModel struct {
 type Message struct {
 	Id      int       `json:"message_id"`
 	Message string    `json:"message"`
+	Blocked bool      `json:"blocked"`
 	Time    time.Time `json:"date"`
 	User    User      `json:"user"`
 }
 
-func (m *MessageModel) GetAll() ([]Message, error) {
+func (m *MessageModel) GetAll(userId int) ([]Message, error) {
 	var messages []Message
-	var queryString = `select messages.id , users.id, "username", "avatar_color", "avatar_url", "message_body", date from "messages"
-	inner join "users" on messages.user_id = users.id
-	inner join "avatars" on users.avatar = avatars.id`
-	res, err := m.DB.Query(queryString)
+	var queryString = `
+	select
+	m.id,
+	m.message_body,
+	m.date,
+	m.user_id,
+	"username",
+	"admin",
+	"avatar_url",
+	"avatar_color",
+	CASE
+	    WHEN b.blocked_user_id IS NOT NULL THEN TRUE
+	    ELSE FALSE
+	END AS blocked
+	FROM "messages" m
+	LEFT JOIN blocked_users b
+	ON m.user_id = b.blocked_user_id
+	AND b.user_id = $1
+	INNER JOIN users on m.user_id = users.id
+	INNER JOIN avatars on avatar = avatars.id
+	`
+	res, err := m.DB.Query(queryString, userId)
 
 	if err != nil {
 		return messages, err
@@ -31,8 +50,17 @@ func (m *MessageModel) GetAll() ([]Message, error) {
 
 	for res.Next() {
 		message := &Message{}
-		err := res.Scan(&message.Id, &message.User.Id, &message.User.Username,
-			&message.User.Avatar.Color, &message.User.Avatar.Url, &message.Message, &message.Time)
+		err := res.Scan(
+			&message.Id,
+			&message.Message,
+			&message.Time,
+			&message.User.Id,
+			&message.User.Username,
+			&message.User.Admin,
+			&message.User.Avatar.Url,
+			&message.User.Avatar.Color,
+			&message.Blocked,
+		)
 
 		if err != nil {
 			return messages, err
@@ -42,14 +70,30 @@ func (m *MessageModel) GetAll() ([]Message, error) {
 	return messages, nil
 }
 
-func (m *MessageModel) Get(filters filter.Filter) ([]Message, error) {
+func (m *MessageModel) Get(filters filter.Filter, userId int) ([]Message, error) {
 	var messages []Message
 	var queryString = `
-	select messages.id , users.id, "admin", "username", "avatar_color", "avatar_url", "message_body", date from "messages"
-	inner join "users" on messages.user_id = users.id
-	inner join "avatars" on users.avatar = avatars.id
-	where "message_body" ilike $1 and "username" ilike $2
-	limit $3 offset $4`
+	select
+	m.id,
+	m.user_id,
+	m.message_body,
+	m.date,
+	"username",
+	"admin",
+	"avatar_url",
+	"avatar_color",
+	CASE
+	    WHEN b.blocked_user_id IS NOT NULL THEN TRUE
+	    ELSE FALSE
+	END AS blocked
+	FROM "messages" m
+	LEFT JOIN blocked_users b
+	ON m.user_id = b.blocked_user_id
+	AND b.user_id = 17
+	INNER JOIN users on m.user_id = users.id
+	INNER JOIN avatars on users.avatar = avatars.id
+	WHERE m.message_body ILIKE $1 and "username" ILIKE $2
+	limit $3 offset $4;`
 	res, err := m.DB.Query(queryString, "%"+filters.Keyword+"%", "%"+filters.Username+"%", filters.Page_Size, filters.CurrentPage())
 
 	if err != nil {
@@ -58,8 +102,17 @@ func (m *MessageModel) Get(filters filter.Filter) ([]Message, error) {
 
 	for res.Next() {
 		var message Message
-		res.Scan(&message.Id, &message.User.Id, &message.User.Admin, &message.User.Username,
-			&message.User.Avatar.Color, &message.User.Avatar.Url, &message.Message, &message.Time)
+		res.Scan(
+			&message.Id,
+			&message.User.Id,
+			&message.Message,
+			&message.Time,
+			&message.User.Username,
+			&message.User.Admin,
+			&message.User.Avatar.Url,
+			&message.User.Avatar.Color,
+			&message.Blocked,
+		)
 		messages = append(messages, message)
 	}
 
