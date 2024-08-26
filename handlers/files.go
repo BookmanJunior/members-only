ee83d4f267502e25970f18b6437edb29b5e3ae73
Gulov Azam <gulovazam@gmail.com>
2024-08-26 18:14:38 +0500

    Add HandleGetMessagesAsPdf function

--- handlers/files.go
+++ handlers/files.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
@@ -9,7 +10,39 @@ import (
 	"github.com/bookmanjunior/members-only/config"
 	"github.com/bookmanjunior/members-only/internal/auth"
 	"github.com/bookmanjunior/members-only/internal/filter"
+	"github.com/bookmanjunior/members-only/internal/pdf"
 )
+
+func HandleGetMessagesAsPdf(a *config.Application) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		page, err := strconv.Atoi(r.URL.Query().Get("page"))
+		currentUser := r.Context().Value("current_user").(auth.UserClaim)
+
+		if err != nil {
+			clientError(w, a, err, http.StatusBadRequest, map[string]string{"error": "Wrong page number"})
+			return
+		}
+
+		filters := filter.Filter{
+			Page:      page,
+			Page_Size: 10,
+			Keyword:   r.URL.Query().Get("keyword"),
+			Username:  r.URL.Query().Get("username"),
+			Order:     r.URL.Query().Get("order"),
+		}
+
+		messages, err := a.Messages.Get(filters, currentUser.Id)
+
+		fileName := "messages" + strconv.Itoa(rand.Intn(100)) + ".pdf"
+		pdf.Generate(messages, fileName)
+
+		defer os.RemoveAll(fileName)
+
+		w.Header().Set("Content-Disposition", "attachment; filename=messages.pdf")
+		w.Header().Set("Content-Type", "application/pdf")
+		http.ServeFile(w, r, fileName)
+	}
+}
 
 func HandleGetMessagesAsFile(a *config.Application) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
