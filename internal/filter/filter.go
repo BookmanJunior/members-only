b1eaac1502e4133366246187eb597bdd7f7dfb5c
Gulov Azam <gulovazam@gmail.com>
2024-08-27 20:37:03 +0500

    Add MetaData struct
    Add CalculateMetaData function

--- internal/filter/filter.go
+++ internal/filter/filter.go
@@ -1,5 +1,7 @@
 package filter
 
+import "math"
+
 type Filter struct {
 	Order     string `json:"order,omitempty"`
 	Username  string `json:"username,omitempty"`
@@ -10,4 +12,26 @@ type Filter struct {
 
 func (f Filter) CurrentPage() int {
 	return (f.Page - 1) * f.Page_Size
+}
+
+type MetaData struct {
+	Page         int
+	PageSize     int
+	FirstPage    int
+	LastPage     int
+	TotalRecords int
+}
+
+func CalculateMetaData(totalRecords, page, pageSize int) MetaData {
+	if totalRecords == 0 {
+		return MetaData{}
+	}
+
+	return MetaData{
+		Page:         page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
 }
