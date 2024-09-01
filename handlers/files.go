73bd1f3f3253577a4408ff2724c65c8e6774de25
Gulov Azam <gulovazam@gmail.com>
2024-09-02 03:48:41 +0500

    Remove HandleGetMessagesAsFile handler

--- handlers/files.go
+++ handlers/files.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"math/rand"
 	"net/http"
 	"os"
@@ -41,58 +40,5 @@ func HandleGetMessagesAsPdf(a *config.Application) http.HandlerFunc {
 		w.Header().Set("Content-Disposition", "attachment; filename=messages.pdf")
 		w.Header().Set("Content-Type", "application/pdf")
 		http.ServeFile(w, r, fileName)
-	}
-}
-
-func HandleGetMessagesAsFile(a *config.Application) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		error := map[string]string{"error": "Error creating file"}
-		currentUser := r.Context().Value("current_user").(auth.UserClaim)
-
-		page, err := strconv.Atoi(r.URL.Query().Get("page"))
-
-		if err != nil || page < 1 || page > 1000 {
-			clientError(w, a, err, http.StatusBadRequest, map[string]string{"error": "Wrong page number"})
-			return
-		}
-
-		filters := filter.Filter{
-			Page:      page,
-			Page_Size: 10,
-			Keyword:   r.URL.Query().Get("keyword"),
-			Username:  r.URL.Query().Get("username"),
-			Order:     r.URL.Query().Get("order"),
-		}
-
-		messages, _, err := a.Messages.Get(filters, currentUser.Id)
-
-		file, err := os.CreateTemp(".", "messages*.json")
-
-		if err != nil {
-			clientError(w, a, err, http.StatusInternalServerError, error)
-			return
-		}
-
-		defer file.Close()
-		defer os.Remove(file.Name())
-
-		d, err := json.MarshalIndent(messages, "", "   ")
-
-		if err != nil {
-			clientError(w, a, err, http.StatusInternalServerError, error)
-			return
-		}
-
-		_, err = file.Write(d)
-
-		if err != nil {
-			clientError(w, a, err, http.StatusInternalServerError, error)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Content-Disposition", "attachment; filename=messages.json")
-		http.ServeFile(w, r, file.Name())
-
 	}
 }
