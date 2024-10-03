4a3596edb33550c153a24c27f069b5cc46de95f0
Gulov Azam <gulovazam@gmail.com>
2024-10-04 01:15:25 +0500

    Move error related functionsto a separate error module
    add parseIdParam function

--- handlers/helpers.go
+++ handlers/helpers.go
@@ -2,11 +2,9 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"net/http"
-	"runtime/debug"
-
-	"github.com/bookmanjunior/members-only/config"
+	"strconv"
 )
 
 type CustomError map[string]any
@@ -18,38 +16,10 @@ func WriteJSON(w http.ResponseWriter, code int, v any) error {
 	return json.NewEncoder(w).Encode(v)
 }
 
-func responseError(w http.ResponseWriter, status int, message any) {
-	customError := CustomError{"message": message}
-
-	if err := WriteJSON(w, status, customError); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+func parseIdParam(id string) (int, error) {
+	parsedId, err := strconv.Atoi(id)
+	if err != nil || parsedId < 1 {
+		return 0, errors.New("invalid id parameter")
 	}
-}
-
-func serverError(w http.ResponseWriter, app *config.Application, err error) {
-	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.ErrorLog.Println(trace)
-
-	responseError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
-}
-
-func clientError(w http.ResponseWriter, status int, message any) {
-	responseError(w, status, message)
-}
-
-func notFound(w http.ResponseWriter) {
-	clientError(w, http.StatusNotFound, "Resource not found")
-
-}
-
-func badCredentials(w http.ResponseWriter) {
-	clientError(w, http.StatusForbidden, "Wrong username or password")
-}
-
-func Unauthorized(w http.ResponseWriter) {
-	clientError(w, http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized))
-}
-
-func Forbidden(w http.ResponseWriter) {
-	clientError(w, http.StatusForbidden, http.StatusText(http.StatusForbidden))
+	return parsedId, nil
 }
