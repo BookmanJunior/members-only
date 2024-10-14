5072f52603deb58384d6e0145c92c5fd63cdcd3e
Gulov Azam <gulovazam@gmail.com>
2024-10-15 02:17:42 +0500

    return server from CreateServerTx
    join user avatars on GetById
    join server type

--- internal/models/servers.go
+++ internal/models/servers.go
@@ -23,8 +23,10 @@ func (s ServerModel) GetUsersServers(userId int) ([]Server, error) {
 		`
 	select distinct on(server_id) s.server_id,
 	s.server_name,
-	s.server_img
+	s.server_img,
+	po.type
 	from servers s
+	join privacy_options po on po.id = s.type
 	where exists (
 	select * from server_members sm where user_id = $1
 	)
@@ -40,6 +42,7 @@ func (s ServerModel) GetUsersServers(userId int) ([]Server, error) {
 			&server.Id,
 			&server.Name,
 			&server.Icon,
+			&server.Type,
 		)
 		if err != nil {
 			return nil, err
@@ -58,18 +61,23 @@ func (s ServerModel) GetById(serverId int) (Server, error) {
 	s.server_id,
 	s.server_name,
 	s.server_img,
+	po.type,
 	ch.channel_id,
 	ch.channel_name
 	from servers s
+	join privacy_options po on po.id = s.type
 	join channels ch on ch.server_id = $1 and s.server_id = $1
 	`
 	membersQueryString :=
 		`
-	select 
+	select
 	u.user_id,
-	u.username
+	u.username,
+	a.avatar_color,
+	a.avatar_url
 	from server_members sm
 	join users u on u.user_id = sm.user_id and sm.server_id = $1
+	join avatars a on a.avatar_id = u.avatar
 	`
 	res, err := s.DB.Query(channelQueryString, serverId)
 	if err != nil {
@@ -82,6 +90,7 @@ func (s ServerModel) GetById(serverId int) (Server, error) {
 			&server.Id,
 			&server.Name,
 			&server.Icon,
+			&server.Type,
 			&channel.Id,
 			&channel.Name,
 		)
@@ -101,6 +110,8 @@ func (s ServerModel) GetById(serverId int) (Server, error) {
 		err = usersRes.Scan(
 			&user.Id,
 			&user.Username,
+			&user.Avatar.Color,
+			&user.Avatar.Url,
 		)
 		if err != nil {
 			return server, err
@@ -111,15 +122,15 @@ func (s ServerModel) GetById(serverId int) (Server, error) {
 	return server, nil
 }
 
-func (s ServerModel) CreateServerTx(name, icon string, ownerId int) (Server, error) {
-	var server Server
+func (s ServerModel) CreateServerTx(name, icon string, ownerId int) (int, error) {
+	var serverId int
 	// null represents output returned by the procedure
-	err := s.DB.QueryRow("call createNewServer($1, $2, $3, null)", name, icon, ownerId).Scan(&server.Id)
+	err := s.DB.QueryRow("call createNewServer($1, $2, $3, null)", name, icon, ownerId).Scan(&serverId)
 	if err != nil {
-		return server, err
+		return serverId, err
 	}
 
-	return server, nil
+	return serverId, nil
 
 }
 
