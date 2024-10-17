7526a9b3d36e3b8bc56b9e34c334f5c3fc6a9edc
Gulov Azam <gulovazam@gmail.com>
2024-10-17 23:30:27 +0500

    Add routes

--- api/routes.go
+++ api/routes.go
@@ -25,6 +25,9 @@ func Router(app *config.Application) http.Handler {
 	router.HandleFunc("POST /invite", middleware.IsAuthorized(app, handlers.HandleServerInvitation(app)))
 	router.HandleFunc("GET /invite/{link}", middleware.IsAuthorized(app, handlers.CheckInvite(app)))
 	router.HandleFunc("GET /servers/{id}/{channelId}", middleware.IsAuthorized(app, handlers.HandleGetChannel(app)))
+	router.HandleFunc("POST /servers/{id}/channel", middleware.IsAuthorized(app, handlers.HandleCreateChannel(app)))
+	router.HandleFunc("PATCH /servers/{id}/{channelId}", middleware.IsAuthorized(app, handlers.HandleUpdateChannel(app)))
+	router.HandleFunc("DELETE /servers/{id}/{channelId}", middleware.IsAuthorized(app, handlers.HandleDeleteChannel(app)))
 	router.HandleFunc("GET /feed", middleware.IsAuthorized(app, handlers.HandleGetFeed(app)))
 	router.HandleFunc("GET /ws", middleware.IsAuthorized(app, handlers.HandleWs(app)))
 
