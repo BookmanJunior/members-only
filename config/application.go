87c72209848a961f988726d2d1a0bc74862df625
Gulov Azam <gulovazam@gmail.com>
2024-10-17 23:30:14 +0500

    Update list of dependencies

--- config/application.go
+++ config/application.go
@@ -6,16 +6,20 @@ import (
 	"github.com/bookmanjunior/members-only/internal/cloud"
 	"github.com/bookmanjunior/members-only/internal/hub"
 	"github.com/bookmanjunior/members-only/internal/models"
+	"github.com/redis/go-redis/v9"
 )
 
 type Application struct {
-	ErrorLog *log.Logger
-	InfoLog  *log.Logger
-	Users    *models.UserModel
-	Messages *models.MessageModel
-	Avatar   *models.AvatarModel
-	Servers  *models.ServerModel
-	Channels *models.ChannelModel
-	Cloud    cloud.Cloud
-	Hub      *hub.Hub
+	ErrorLog       *log.Logger
+	InfoLog        *log.Logger
+	Users          *models.UserModel
+	Messages       *models.MessageModel
+	Avatar         *models.AvatarModel
+	Servers        *models.ServerModel
+	ServerMembers  *models.ServerMembersModel
+	ServerMessages *models.ServerMessageModel
+	Channels       *models.ChannelModel
+	Redis          *redis.Client
+	Cloud          cloud.Cloud
+	Hub            *hub.Hub
 }
