d63590e38cf7eb908a5d84796081621792e8bf76
Gulov Azam <gulovazam@gmail.com>
2024-10-18 13:26:29 +0500

    Refactor code

--- internal/hub/handlers.go
+++ internal/hub/handlers.go
@@ -1,21 +1,35 @@
 package hub
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/bookmanjunior/members-only/internal/models"
 )
 
 func HandleWSMessagePost(client *Client, msg *WSMessage) {
 	var responseMessage WSResponseMessage
-	res, err := client.DB.Insert(msg.Message, client.User.Id)
+	newServerMessage := models.ServerMessage{
+		ServerId:  msg.ServerID,
+		ChannelId: msg.ChannelId,
+		Message:   msg.Message,
+		SentDate:  time.Now(),
+		User:      client.User,
+	}
+
+	newServerMessage.User.Servers = nil
+
+	res, err := client.DB.Insert(newServerMessage)
 	if err != nil {
 		ServerError(&responseMessage)
+		responseMessage.Data.User.Id = client.User.Id
 		client.Hub.Broadcast <- responseMessage
 		return
 	}
 
-	responseMessage.Type = "success"
-	responseMessage.Message = "Successfully posted your message"
+	responseMessage.Type = "message"
 	responseMessage.StatusCode = http.StatusOK
 	responseMessage.Data = res
 
@@ -24,45 +38,53 @@ func HandleWSMessagePost(client *Client, msg *WSMessage) {
 
 func HandleWSMessageUpdate(client *Client, msg *WSMessage) {
 	var responseMessage WSResponseMessage
+	responseMessage.Data.ServerId = msg.ServerID
+	responseMessage.Data.ChannelId = msg.ChannelId
+	responseMessage.Data.Id = msg.MessageId
+	responseMessage.Data.User = client.User
+	responseMessage.Data.User.Servers = nil
 
-	if client.User.Id != msg.UserId {
-		UnauthorizedError(&responseMessage)
-		client.Hub.Broadcast <- responseMessage
-		return
-	}
-
-	updatedMessage, err := client.DB.UpdateMessage(msg.MessageId, msg.Message)
+	updatedMessage, err := client.DB.Update(msg.Message, msg.MessageId, client.User.Id)
 	if err != nil {
+		if err == sql.ErrNoRows {
+			BadRequestError(&responseMessage)
+			client.Hub.Broadcast <- responseMessage
+			return
+		}
+		responseMessage.Data.User.Id = client.User.Id
 		ServerError(&responseMessage)
 		client.Hub.Broadcast <- responseMessage
 		return
 	}
 
-	responseMessage.Type = "success"
-	responseMessage.Message = "Successfully updated message"
+	responseMessage.Type = "message"
 	responseMessage.StatusCode = http.StatusOK
 	responseMessage.Data = updatedMessage
+	responseMessage.Headers.Method = "PATCH"
 	client.Hub.Broadcast <- responseMessage
 }
 
 func HandleWsMessageDelete(client *Client, msg *WSMessage) {
 	var responseMessage WSResponseMessage
+	responseMessage.Data.ServerId = msg.ServerID
+	responseMessage.Data.ChannelId = msg.ChannelId
+	responseMessage.Data.Id = msg.MessageId
+	responseMessage.Data.User = client.User
+	responseMessage.Data.User.Servers = nil
 
-	if client.User.Id != msg.UserId {
-		UnauthorizedError(&responseMessage)
-		client.Hub.Broadcast <- responseMessage
-		return
-	}
-
-	err := client.DB.Delete(msg.MessageId)
+	_, err := client.DB.Delete(msg.MessageId, msg.UserId)
 	if err != nil {
+		if err == sql.ErrNoRows {
+			UnauthorizedError(&responseMessage)
+			client.Hub.Broadcast <- responseMessage
+			return
+		}
 		ServerError(&responseMessage)
 		client.Hub.Broadcast <- responseMessage
 		return
 	}
 
-	responseMessage.Type = "success"
-	responseMessage.Message = "Successfully deleted message"
+	responseMessage.Type = "message"
 	responseMessage.StatusCode = http.StatusOK
 	responseMessage.Message = fmt.Sprintf("Deleted message: %v", msg.MessageId)
 	client.Hub.Broadcast <- responseMessage
